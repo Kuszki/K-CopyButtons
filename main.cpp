baab53289dd011d8866309ab656d9994be5e3f71
Kuszki <l.drozdz@o2.pl>
2015-09-01 12:25:36 +0200

    Prepare to update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.hpp"
 
+#include <QLibraryInfo>
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication a(argc, argv); MainWindow w;
 
-	MainWindow w;
+	QTranslator qtTranslator;
+	qtTranslator.load("qt_" + QLocale::system().name(),
+				   QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	a.installTranslator(&qtTranslator);
+
 	w.show();
 
 	return a.exec();
